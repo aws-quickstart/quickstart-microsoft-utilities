c2475ad845f2ff4ab5b7b9a036e00afbd8f2d2d6
Santiago Cardenas <sancard@amazon.com>
2016-05-03 18:28:30 -0700

    Added S3 URI file download support (via existing IAM role)

--- scripts/Download-File.ps1
+++ scripts/Download-File.ps1
@@ -7,6 +7,26 @@ param(
     [string]$Destination
 )
 
+function Get-S3BucketName {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$S3Uri
+    )
+
+    return ($S3Uri -split '/')[2]
+}
+
+function Get-S3Key {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$S3Uri
+    )
+
+    $bucketName = Get-S3BucketName -S3Uri $S3Uri
+
+    return $S3Uri.Substring(("s3://$bucketName/").Length)
+}
+
 try {
     $ErrorActionPreference = "Stop"
 
@@ -15,7 +35,14 @@ try {
         New-Item -Path $parentDir -ItemType directory -Force | Out-Null
     }
 
-    (New-Object System.Net.WebClient).DownloadFile($Source,$Destination)
+    $qualifier = Split-Path $Source -Qualifier
+    if ($qualifier -eq "s3:") {
+        Read-S3Object -BucketName (Get-S3BucketName -S3Uri $Source) -Key (Get-S3Key -S3Uri $Source) -File $Destination
+    } elseif ($qualifier -in ("http:","https:")) {
+        (New-Object System.Net.WebClient).DownloadFile($Source,$Destination)
+    } else {
+        throw "$Source is not a valid S3, HTTP, or HTTPS URI"
+    }
 }
 catch {
     $_ | Write-AWSQuickStartException
