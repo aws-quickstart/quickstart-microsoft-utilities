9eddd6050d6e71faf7862ae9e6d0ff083066988e
Jay McConnell <jmmccon@amazon.com>
2016-09-30 10:26:19 +0200

    $FullAccessUser in script block needs $Using:

--- scripts/Create-Share.ps1
+++ scripts/Create-Share.ps1
@@ -46,7 +46,7 @@ try {
 
     $CreateSharePs={
         $ErrorActionPreference = "Stop"
-        New-SmbShare -Name $Using:ShareName -Path $Using:Path -FullAccess $FullAccessUser
+        New-SmbShare -Name $Using:ShareName -Path $Using:Path -FullAccess $Using:FullAccessUser
     }
     Invoke-Command -Scriptblock $CreateSharePs -ComputerName $ServerName -Credential $DomainAdminCreds
 }
