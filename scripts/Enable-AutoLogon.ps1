fad4656a09a33163b24da6d2634adbc1116440e8
Philip Smith <philip.smith@nordcloud.com>
2017-06-26 22:48:39 +0100

    adding checks for registry values

--- scripts/Enable-AutoLogon.ps1
+++ scripts/Enable-AutoLogon.ps1
@@ -14,9 +14,24 @@ param (
 )
 
 try {
-    New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoAdminLogon -Value 1 -ErrorAction Stop | Out-Null
-    New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultUserName -Value $UserName -ErrorAction Stop | Out-Null
-    New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultPassword -Value $Password -ErrorAction Stop | Out-Null
+    $winlogon = Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+
+    # Check for the AutoAdminLogon value and it's current status before applying.
+    If(-Not($winlogon.GetValue("AutoAdminLogon") -eq "1")) {
+        New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoAdminLogon -Value 1 -ErrorAction Stop | Out-Null
+    } else { write "AutoAdminLogon already set to 1" }
+
+    # Check for the DefaultUserName before applying
+    If(-Not($winlogon.GetValue("DefaultUserName").split('@')[0] -eq $UserName)) {
+        New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultUserName -Value $UserName -ErrorAction Stop | Out-Null
+    } else { write "DefaultUserName is already set" }
+
+    # Check for the DefaultPassword before applying
+    If(-Not($winlogon.GetValue("DefaultPassword") -eq "$Password")) {
+        New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultPassword -Value $Password -ErrorAction Stop | Out-Null
+    } else { write "DefaultPassword already set" }
+
+    # Add a startup script if specified
     if (-not [string]::IsNullOrEmpty($StartupScript)) {
         New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name Install -Value $StartupScript -ErrorAction Stop | Out-Null
     }
