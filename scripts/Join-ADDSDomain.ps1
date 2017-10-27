175593afd0e45d0fbea94e830c7aec3a27b55acc
Santiago Cardenas <sancard@amazon.com>
2017-10-27 15:46:06 -0700

    Add new Restart-Computer script. Update shutdown to check for exit code.

--- scripts/Join-ADDSDomain.ps1
+++ scripts/Join-ADDSDomain.ps1
@@ -44,7 +44,10 @@ try {
 
     if ($Restart) {
         # Execute restart after script exit and allow time for external services
-        Invoke-Expression -Command "shutdown.exe /r /t 10"
+        $shutdown = Start-Process -FilePath "shutdown.exe" -ArgumentList @("/r", "/t 10") -Wait -NoNewWindow -PassThru
+        if ($shutdown.ExitCode -ne 0) {
+            throw "[ERROR] shutdown.exe exit code was not 0. It was actually $($shutdown.ExitCode)."
+        }
     }
 }
 catch {
--- scripts/Restart-Computer.ps1
+++ scripts/Restart-Computer.ps1
@@ -1,22 +1,8 @@
 ﻿[CmdletBinding()]
-param(
-    [string]
-    $DomainName,
-
-    [string]
-    $UserName,
-
-    [string]
-    $Password
-)
+param()
 
 try {
     $ErrorActionPreference = "Stop"
-
-    $pass = ConvertTo-SecureString $Password -AsPlainText -Force
-    $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName,$pass
-
-    Add-Computer -DomainName $DomainName -Credential $cred -ErrorAction Stop
 
     # Execute restart after script exit and allow time for external services
     $shutdown = Start-Process -FilePath "shutdown.exe" -ArgumentList @("/r", "/t 10") -Wait -NoNewWindow -PassThru
