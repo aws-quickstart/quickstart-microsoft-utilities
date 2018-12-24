79324b302499beb5d17ec78de6aeaffbcdf8a298
Aaron Lima <aarolima@amazon.com>
2018-12-24 13:48:50 -0500

    added WMF51 Download\Install Script

---
+++ scripts/Install-WMF51.ps1
@@ -0,0 +1,47 @@
+﻿[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$false)]
+    [string]$Source = 'https://go.microsoft.com/fwlink/?linkid=839516',
+
+    [Parameter(Mandatory=$false)]
+    [string]$Destination = 'C:\cfn\downloads\Win8.1AndW2K12R2-KB3191564-x64.msu'
+)
+
+try {
+    $ErrorActionPreference = "Stop"
+
+    $parentDir = Split-Path $Destination -Parent
+    if (-not (Test-Path $parentDir)) {
+        New-Item -Path $parentDir -ItemType directory -Force | Out-Null
+    }
+
+    Write-Verbose "Trying to download from $Source"
+    $tries = 5
+    while ($tries -ge 1) {
+        try {
+            (New-Object System.Net.WebClient).DownloadFile($Source,$Destination)
+            break
+        }
+        catch {
+            $tries--
+            Write-Verbose "Exception:"
+            Write-Verbose "$_"
+            if ($tries -lt 1) {
+                throw $_
+            }
+            else {
+                Write-Verbose "Failed download. Retrying again in 5 seconds"
+                Start-Sleep 5
+            }
+        }
+    }
+    
+    if ([System.IO.Path]::GetExtension($Destination) -eq '.msu') {
+        Start-Process -FilePath wusa.exe -ArgumentList $Destination,'/quiet','/norestart' -Wait
+    } else {
+        throw "Unsupported file extension"
+    }
+}
+catch {
+    $_ | Write-AWSQuickStartException
+}
